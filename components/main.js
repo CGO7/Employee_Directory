6343ff431cce0d1e5bf62b25e1134cab7859c980
Chris Goslin <cgoslin7@gmail.com>
2021-06-15 08:24:42 -0500

    added components of the DOM

---
+++ components/main.js
@@ -0,0 +1,13 @@
+  
+import React, { Component } from "react";
+import DataArea from "./dataArea";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <DataArea />
+      </>
+    );
+  }
+}
